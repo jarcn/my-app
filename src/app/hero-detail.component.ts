f5ecc1842a98d8f4a9a51de47a42d1d487a4ea42
dorayo <yomingyo@gmail.com>
2018-01-06 01:31:19 +0800

    add options of hreo crud

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -33,4 +33,9 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
 }
